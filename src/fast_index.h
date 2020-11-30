19b150cb91ed92a181e10c3ce1c77318a26aa989
Pierre Lestringant <pierre.lestringant@gmail.com>
2020-11-30 19:17:33 +0100

    [Minor] variable rename

--- src/fast_index.h
+++ src/fast_index.h
@@ -9,21 +9,21 @@ struct fast_index {
 	void* root[0x10000];
 };
 
-int fast_index_insert(struct fast_index* ir, const uint8_t* value);
-int fast_index_insert_buffer(struct fast_index* ir, const uint8_t* buffer, size_t size);
+int fast_index_insert(struct fast_index* fi, const uint8_t* value);
+int fast_index_insert_buffer(struct fast_index* fi, const uint8_t* buffer, size_t size);
 
-uint64_t fast_index_count(struct fast_index* ir);
+uint64_t fast_index_count(struct fast_index* fi);
 
-int fast_index_get_first(struct fast_index* ir, uint8_t* value, size_t mask_size);
-int fast_index_get_next(struct fast_index* ir, uint8_t* value, size_t mask_size);
+int fast_index_get_first(struct fast_index* fi, uint8_t* value, size_t mask_size);
+int fast_index_get_next(struct fast_index* fi, uint8_t* value, size_t mask_size);
 
-void fast_index_compare(struct fast_index* ir, const uint8_t* value);
-void fast_index_compare_buffer(struct fast_index* ir, const uint8_t* buffer, size_t size);
+void fast_index_compare(struct fast_index* fi, const uint8_t* value);
+void fast_index_compare_buffer(struct fast_index* fi, const uint8_t* buffer, size_t size);
 
-int fast_index_intersect(struct fast_index* ir);
-int fast_index_exclude(struct fast_index* ir);
+int fast_index_intersect(struct fast_index* fi);
+int fast_index_exclude(struct fast_index* fi);
 
-void fast_index_clean(struct fast_index* ir);
-void fast_index_delete(struct fast_index* ir);
+void fast_index_clean(struct fast_index* fi);
+void fast_index_delete(struct fast_index* fi);
 
 #endif
