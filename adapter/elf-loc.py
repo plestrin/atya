46de8c0f1fdfe41841497be95e78ceafd7c0b389
Pierre Lestringant <pierre.lestringant@gmail.com>
2021-05-18 18:12:35 +0200

    [Minor] elf-loc.py is now standalone

--- adapter/elf-loc.py
+++ adapter/elf-loc.py
@@ -2,18 +2,17 @@ import sys
 
 from elftools.elf.elffile import ELFFile
 
+from atya import atya_iter
+
 if len(sys.argv) != 2:
 	sys.stderr.write('[-] usage: FILE\n')
 	exit(-1)
-
-patterns = sys.stdin.readlines()
-patterns = [p.strip().decode('hex') for p in patterns]
 
 with open(sys.argv[1], 'rb') as f:
 	data = f.read()
 	elf = ELFFile(f)
 
-	for p in patterns:
+	for p in atya_iter():
 		offset = 0
 		found = False
 		while True:
@@ -24,12 +23,12 @@ with open(sys.argv[1], 'rb') as f:
 			for section in elf.iter_sections():
 				if section.header['sh_type'] in ('SHT_NULL', 'SHT_NOBITS'):
 					continue
-				if section.header['sh_offset'] <= offset and section.header['sh_offset'] + section.header['sh_size'] > offset:
+				if section.header['sh_offset'] <= offset < section.header['sh_offset'] + section.header['sh_size']:
 					sys.stdout.write('%s+%06x ' % (section.name, offset - section.header['sh_offset']))
 
 			found = True
 			offset += 1
 		if not found:
-			sys.stderr.write('[-] unable to find %s in %s' % (p.encode('hex'), sys.argv[1]))
+			sys.stderr.write('[-] unable to find %s in %s' % p.encode('hex'), sys.argv[1])
 		else:
-			sys.stdout.write('%s\n' % (repr(p)))
+			sys.stdout.write('%s\n' % repr(p))
